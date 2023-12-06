f50a4ca667fa2326dc11853252a9f011d2f2e933
Hugo Perrin <b00605838@essec.edu>
2023-12-06 08:30:56 +0000

    [day6] better code, a bit less rigorous...

--- 2023/day6.py
+++ 2023/day6.py
@@ -18,15 +18,10 @@ with open('./data/day6.txt', 'r') as file:
 times = [int(i) for i in re.findall(r'\d+', raw.split('\n')[0])]
 distances = [int(i) for i in re.findall(r'\d+', raw.split('\n')[1])]
 
-# d = lambda x: x*(7 -x)
-# [d(i) for i in range(1,7)]
-
 def solve(t, d):
     delta = t**2 - 4*d
-    x_min, x_max = (t-np.sqrt(delta))/2, (t+np.sqrt(delta))/2
-    int_x_min = np.ceil(x_min if (x_min % 1 != 0) else x_min+1)
-    int_x_max = np.floor(x_max if (x_max % 1 != 0) else x_max-1)
-    return int(int_x_max - int_x_min + 1)
+    x_min, x_max = (t-np.sqrt(delta))/2 + 1e-10, (t+np.sqrt(delta))/2 - 1e-10
+    return int(np.floor(x_max) - np.ceil(x_min) + 1)
 
 #=============================== First part ===============================
 
